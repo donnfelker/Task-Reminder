9f8fff17a65eb5af9541a9ff78e37458f3349656
Donn Felker <dfelker@gmail.com>
2010-07-11 12:35:45 -0500

    initial chekcin of task reminder application

---
+++ src/src/com/dummies/android/taskreminder/ReminderEditActivity.java
@@ -0,0 +1,241 @@
+
+package com.dummies.android.taskreminder;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import com.dummies.android.taskreminder.R;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+public class ReminderEditActivity extends Activity {
+
+	// 
+	// Dialog Constants
+	//
+	private static final int DATE_PICKER_DIALOG = 0;
+	private static final int TIME_PICKER_DIALOG = 1;
+	
+	// 
+	// Date Format 
+	//
+	private static final String DATE_FORMAT = "yyyy-MM-dd"; 
+	private static final String TIME_FORMAT = "kk:mm";
+	private static final String DATE_TIME_FORMAT = DATE_FORMAT + " " + TIME_FORMAT; 
+	
+	private EditText mTitleText;
+    private EditText mBodyText;
+    private Button mDateButton;
+    private Button mTimeButton;
+    private Long mRowId;
+    private RemindersDbAdapter mDbHelper;
+    private Calendar mCalendar;  
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCalendar = Calendar.getInstance(); 
+        mDbHelper = new RemindersDbAdapter(this);
+        mDbHelper.open();
+        setContentView(R.layout.note_edit);
+        
+        
+       
+        mTitleText = (EditText) findViewById(R.id.title);
+        mBodyText = (EditText) findViewById(R.id.body);
+        mDateButton = (Button) findViewById(R.id.reminder_date);
+        mTimeButton = (Button) findViewById(R.id.reminder_time);
+      
+        Button confirmButton = (Button) findViewById(R.id.confirm);
+       
+        mRowId = savedInstanceState != null ? savedInstanceState.getLong(RemindersDbAdapter.KEY_ROWID) 
+                							: null;
+		if (mRowId == null) {
+			Bundle extras = getIntent().getExtras();            
+			mRowId = extras != null ? extras.getLong(RemindersDbAdapter.KEY_ROWID) 
+									: null;
+		}
+
+		populateFields();
+		
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+
+        	public void onClick(View view) {
+        	    setResult(RESULT_OK);
+        	    finish();
+        	}
+          
+        });
+        
+        
+        registerButtonListenersAndSetDefaultText();
+    }
+    
+    @Override
+    protected Dialog onCreateDialog(int id) {
+    	switch(id) {
+    		case DATE_PICKER_DIALOG: 
+    			return showDatePicker();
+    		case TIME_PICKER_DIALOG: 
+    			return showTimePicker(); 
+    	}
+    	return super.onCreateDialog(id);
+    }
+    
+    private TimePickerDialog showTimePicker() {
+		
+    	TimePickerDialog timePicker = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+			
+			@Override
+			public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+				mCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+				mCalendar.set(Calendar.MINUTE, minute); 
+				updateTimeButtonText(); 
+			}
+		}, mCalendar.get(Calendar.HOUR_OF_DAY), mCalendar.get(Calendar.MINUTE), true); 
+		
+    	return timePicker; 
+	}
+
+	private DatePickerDialog showDatePicker() {
+		
+		
+		DatePickerDialog datePicker = new DatePickerDialog(ReminderEditActivity.this, new DatePickerDialog.OnDateSetListener() {
+			
+			@Override
+			public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+				mCalendar.set(Calendar.YEAR, year);
+				mCalendar.set(Calendar.MONTH, monthOfYear);
+				mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+				updateDateButtonText(); 
+			}
+		}, mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH), mCalendar.get(Calendar.DAY_OF_MONTH)); 
+		return datePicker; 
+	}
+
+	private void registerButtonListenersAndSetDefaultText() {
+
+		mDateButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				showDialog(DATE_PICKER_DIALOG);  
+			}
+		}); 
+		
+		
+		mTimeButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				showDialog(TIME_PICKER_DIALOG); 
+			}
+		}); 
+	}
+
+    
+    private void populateFields()  {
+    	
+    	// Get a calendar object which starts with todays date.
+    	
+    	
+    	// Only populate the text boxes and change the calendar date
+    	// if the row is not null from the database. 
+        if (mRowId != null) {
+            Cursor note = mDbHelper.fetchReminder(mRowId);
+            startManagingCursor(note);
+            mTitleText.setText(note.getString(
+    	            note.getColumnIndexOrThrow(RemindersDbAdapter.KEY_TITLE)));
+            mBodyText.setText(note.getString(
+                    note.getColumnIndexOrThrow(RemindersDbAdapter.KEY_BODY)));
+            
+
+            // Get the date from the database and format it for our use. 
+            SimpleDateFormat dateTimeFormat = new SimpleDateFormat(DATE_TIME_FORMAT);
+            Date date = null;
+			try {
+				String dateString = note.getString(note.getColumnIndexOrThrow(RemindersDbAdapter.KEY_DATE_TIME)); 
+				date = dateTimeFormat.parse(dateString);
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (ParseException e) {
+				e.printStackTrace();
+			} 
+            
+            // Tell the calendar what date we are now going to work with. 
+            mCalendar.setTime(date); 
+
+        } 
+        
+        updateDateButtonText(); 
+        updateTimeButtonText(); 
+        	
+    }
+
+	private void updateTimeButtonText() {
+		// Set the time button text based upon the value from the database
+        SimpleDateFormat timeFormat = new SimpleDateFormat(TIME_FORMAT); 
+        String timeForButton = timeFormat.format(mCalendar.getTime()); 
+        mTimeButton.setText(timeForButton);
+	}
+
+	private void updateDateButtonText() {
+		// Set the date button text based upon the value from the database 
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT); 
+        String dateForButton = dateFormat.format(mCalendar.getTime()); 
+        mDateButton.setText(dateForButton);
+	}
+    
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong(RemindersDbAdapter.KEY_ROWID, mRowId);
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //saveState();
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+		populateFields();
+    }
+    
+    private void saveState() {
+        String title = mTitleText.getText().toString();
+        String body = mBodyText.getText().toString();
+
+        SimpleDateFormat dateTimeFormat = new SimpleDateFormat(DATE_TIME_FORMAT); 
+    	String reminderDateTime = dateTimeFormat.format(mCalendar.getTime());
+        
+        if (mRowId == null) {
+        	
+        	long id = mDbHelper.createReminder(title, body, reminderDateTime);
+            if (id > 0) {
+                mRowId = id;
+            }
+        } else {
+            mDbHelper.updateReminder(mRowId, title, body, reminderDateTime);
+        }
+       
+        // Update AlarmManager stuff 
+        //AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+        //alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, operation)
+    }
+    
+}
---
+++ src/src/com/dummies/android/taskreminder/TaskReminderActivity.java
@@ -0,0 +1,113 @@
+
+package com.dummies.android.taskreminder;
+
+import com.dummies.android.taskreminder.R;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+
+public class TaskReminderActivity extends ListActivity {
+    private static final int ACTIVITY_CREATE=0;
+    private static final int ACTIVITY_EDIT=1;
+    
+    private static final int INSERT_ID = Menu.FIRST;
+    private static final int DELETE_ID = Menu.FIRST + 1;
+
+    private RemindersDbAdapter mDbHelper;
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notes_list);
+        mDbHelper = new RemindersDbAdapter(this);
+        mDbHelper.open();
+        fillData();
+        registerForContextMenu(getListView());
+
+    }
+    
+
+	private void fillData() {
+        Cursor notesCursor = mDbHelper.fetchAllReminders();
+        startManagingCursor(notesCursor);
+        
+        // Create an array to specify the fields we want to display in the list (only TITLE)
+        String[] from = new String[]{RemindersDbAdapter.KEY_TITLE};
+        
+        // and an array of the fields we want to bind those fields to (in this case just text1)
+        int[] to = new int[]{R.id.text1};
+        
+        // Now create a simple cursor adapter and set it to display
+        SimpleCursorAdapter notes = 
+        	    new SimpleCursorAdapter(this, R.layout.notes_row, notesCursor, from, to);
+        setListAdapter(notes);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, INSERT_ID, 0, R.string.menu_insert).setIcon(android.R.drawable.ic_menu_add);
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch(item.getItemId()) {
+        case INSERT_ID:
+            createNote();
+            return true;
+        }
+       
+        return super.onMenuItemSelected(featureId, item);
+    }
+	
+    @Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0, DELETE_ID, 0, R.string.menu_delete);
+	}
+
+    @Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+    	case DELETE_ID:
+    		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+	        mDbHelper.deleteReminder(info.id);
+	        fillData();
+	        return true;
+		}
+		return super.onContextItemSelected(item);
+	}
+	
+    private void createNote() {
+        Intent i = new Intent(this, ReminderEditActivity.class);
+        startActivityForResult(i, ACTIVITY_CREATE);
+    }
+    
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Intent i = new Intent(this, ReminderEditActivity.class);
+        i.putExtra(RemindersDbAdapter.KEY_ROWID, id);
+        startActivityForResult(i, ACTIVITY_EDIT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, 
+                                    Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        fillData();
+    }
+}
